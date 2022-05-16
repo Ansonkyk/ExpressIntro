4db7fb565dd8ee1f5c10d97f1363993493ecdc05
Ansonkyk <anson.kong@codeimmersives.com>
2022-05-16 14:38:46 -0400

    v3

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Anson ld!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+app.get('*', function(req, res){
+  res.status(404).send('eror 404');
+});
